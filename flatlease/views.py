f3bea1b8a0aba47bd34aa1aeda26a98321969d3d
pinballwizard <menstenebris@gmail.com>
2015-10-06 13:44:39 +0700

    create profiles

--- flatlease/views.py
+++ flatlease/views.py
@@ -3,6 +3,67 @@ from flatlease.models import *
 from django import forms
 
 
+class ClientAddForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ['name', 'last_name', 'second_name', 'birthday', 'residence',
+                  'phone', 'email', 'photo', 'passport', 'deposit', 'comment']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'type': 'text',
+                'class': 'mdl-textfield__input',
+                'required': True,
+                'pattern': ".*",
+            }),
+            'last_name': forms.TextInput(attrs={
+                'type': 'text',
+                'class': 'mdl-textfield__input',
+                'required': True,
+            }),
+            'second_name': forms.TextInput(attrs={
+                'type': 'text',
+                'class': 'mdl-textfield__input',
+            }),
+            'residence': forms.TextInput(attrs={
+                'type': 'text',
+                'class': 'mdl-textfield__input',
+                'required': True,
+            }),
+            'phone': forms.TextInput(attrs={
+                'type': 'text',
+                'class': 'mdl-textfield__input',
+                'required': True,
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'mdl-textfield__input',
+            }),
+            'comment': forms.Textarea(attrs={
+                'type': 'text',
+                'class': 'mdl-textfield__input',
+                'rows': 3,
+            }),
+        }
+
+
+class PropertyAddForm(forms.ModelForm):
+    class Meta:
+        model = FixedProperty
+        fields = ['location', 'cost']
+        widgets = {
+            'location': forms.TextInput(attrs={
+                'type': 'text',
+                'class': 'mdl-textfield__input',
+                'required': True,
+                'pattern': ".*",
+            }),
+            'cost': forms.TextInput(attrs={
+                'type': 'text',
+                'class': 'mdl-textfield__input',
+                'required': True,
+            }),
+        }
+
+
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=50)
     search.widget = forms.TextInput(attrs={'placeholder': 'Поиск...', 'class': 'mdl-textfield__input', 'type': 'text'})
@@ -13,7 +74,19 @@ def calculator(request):
 
 
 def addition(request):
-    return render(request, 'flatlease/addition.html')
+    data = {
+        'add_client_form': ClientAddForm(),
+        'add_property_form': PropertyAddForm()
+    }
+    if request.method == 'POST':
+        client_form = ClientAddForm(request.POST)
+        if client_form.is_valid():
+            print("YES")
+            data['add_client_form'] = client_form
+            client_form.save()
+        else:
+            data['add_client_form'] = client_form
+    return render(request, 'flatlease/addition.html', data)
 
 
 def search(request):
