49694d4017857d93e566f3b0f88340ef6fe39e2d
craby-c <securicula@gmail.com>
2015-10-25 12:17:53 +0700

    some fix

--- flatlease/static/flatlease/script/main.js
+++ flatlease/static/flatlease/script/main.js
@@ -24,14 +24,27 @@ $( document ).ready(function(){
 //  clean form
     $(":reset").click(function(){
         $("form").reset();
-    });
+        }
+    );
 
 //  calculator function
     $("#calculate").click(function(){
         var price = $("#price").val();
         var firstpay = $("#first-pay").val();
         var payment = $("#payment").val();
-        var period = Math.ceil((price - firstpay)/payment);
+        var residue = price - firstpay;
+        var period = Math.ceil(residue/payment);
         $(".total").text(period + " месяцев");
+//  filling table
+        var today = new Date();
+        $(".today").text(today.getDate()+"."+today.getMonth()+"."+today.getFullYear());
+        $(".firstpay").text(firstpay);
+        $(".residue").text(residue);
+        function appendTable(){
+            var row = document.createElement("tr");
+            var cell = $("<td></td>").appendTo(row);
+            $("tbody").append(row)
+        }
     });
+
 });
