1946ff09cfee16099a5a17a9e1fcbdd7859dc28e
craby-c <securicula@gmail.com>
2015-10-29 14:20:33 +0700

    table fix

--- flatlease/static/flatlease/script/main.js
+++ flatlease/static/flatlease/script/main.js
@@ -36,25 +36,33 @@ $( document ).ready(function(){
         var period = Math.ceil(residue/payment);
         $(".total").text(period + " месяцев");
 //  filling table
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1;
-        var yyyy = today.getFullYear();
-        $(".today").text(dd+"."+mm+"."+yyyy);
-        $(".firstpay").text(firstpay);
-        $(".residue").text(residue);
-        var residue2 = residue;
-        for(var i=1; i<=period; i++){
-            if (residue2<=payment){
-                payment=residue2;
+        function fullTable(){
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth()+1;
+            var yyyy = today.getFullYear();
+            $(".today").text(dd+"."+mm+"."+yyyy);
+            $(".firstpay").text(firstpay);
+            $(".residue").text(residue);
+            var residue2 = residue;
+            for(var i=1; i<=period; i++){
+                if (residue2<=payment){
+                    payment=residue2;
+                }
+                residue2 -= payment;
+                var nextmonth = new Date(yyyy, mm+i, dd);
+                var row = document.createElement("tr");
+                $("tbody").append(row);
+                $("<td></td>").appendTo(row).text(nextmonth.getDate()+"."+(nextmonth.getMonth()+1)+"."+nextmonth.getFullYear());
+                $("<td></td>").appendTo(row).text(payment);
+                $("<td></td>").appendTo(row).text(residue2);
             }
-            residue2 -= payment;
-            var nextmonth = new Date(yyyy, mm+i, dd);
-            var row = document.createElement("tr");
-            $("tbody").append(row);
-            $("<td></td>").appendTo(row).text(nextmonth.getDate()+"."+(nextmonth.getMonth()+1)+"."+nextmonth.getFullYear());
-            $("<td></td>").appendTo(row).text(payment);
-            $("<td></td>").appendTo(row).text(residue2);
         }
+        function animateTable(){
+            $("table").animate({
+                height:'100%'
+            }, "slow")
+        }
+        fullTable().animateTable()
     });
 });
